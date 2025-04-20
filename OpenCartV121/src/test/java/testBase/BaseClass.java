63e49548e5e3fb534e0469a6418733d65db5ceac
sujatasharma28 <sujatasharma1690@gmail.com>
2025-04-20 19:31:22 +0530

    Adding xml file

--- OpenCartV121/src/test/java/testBase/BaseClass.java
+++ OpenCartV121/src/test/java/testBase/BaseClass.java
@@ -7,8 +7,11 @@ import org.apache.logging.log4j.LogManager; //Log4j
 import org.apache.logging.log4j.Logger; //Log4j
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	
@@ -16,11 +19,19 @@ public class BaseClass {
 	public Logger logger;
 	
 	@BeforeClass
-	public void setup() {
+	@Parameters({"os","browser"})
+	public void setup(String os, String br) {
 		
-		logger= LogManager.getLogger(this.getClass());
+		logger= LogManager.getLogger(this.getClass()); //log4j
 		
-		driver = new ChromeDriver();
+		switch(br.toLowerCase()) {
+		case "chrome" : driver = new ChromeDriver(); break;
+		case "firefox": driver = new FirefoxDriver(); break;
+		case "edge": driver = new EdgeDriver();   break;
+		default : System.out.println("Invalid browser name"); return;
+		}
+		
+		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
